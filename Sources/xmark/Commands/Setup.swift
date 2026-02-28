0df18e553ec8a57c98f7bb2db588c86a9c9dc3f5
Jon Bauer <jon@covelop.org>
2026-02-27 23:42:08 -0800

    Implement xmark setup
    Installs a post-checkout git hook into .git/hooks/post-checkout.
    If a hook already exists, bails with exit code 1 and prints the
    manual snippet for the user to add to their existing hook.
    Sets hook file permissions to 0755 (executable).

--- Sources/xmark/Commands/Setup.swift
+++ Sources/xmark/Commands/Setup.swift
@@ -1,11 +1,45 @@
 import ArgumentParser
+import Foundation
 
 struct Setup: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Install the post-checkout git hook into the current repo."
     )
 
+    static let hookScript = """
+    #!/bin/sh
+    xmark _hook post-checkout "$1" "$2" "$3"
+    """
+
+    static let hookSnippet = #"xmark _hook post-checkout "$1" "$2" "$3""#
+
     func run() throws {
-        print("setup: not yet implemented")
+        let repoRoot = try GitUtilities.repoRoot()
+        let hookPath = repoRoot
+            .appendingPathComponent(".git")
+            .appendingPathComponent("hooks")
+            .appendingPathComponent("post-checkout")
+
+        if FileManager.default.fileExists(atPath: hookPath.path) {
+            print("""
+            xmark setup: A post-checkout hook already exists at .git/hooks/post-checkout.
+            Add the following line to your existing hook to enable xmark:
+
+                \(Self.hookSnippet)
+            """)
+            throw ExitCode.failure
+        }
+
+        // Write the hook
+        try Self.hookScript.write(to: hookPath, atomically: true, encoding: .utf8)
+
+        // Make it executable (chmod +x)
+        try FileManager.default.setAttributes(
+            [.posixPermissions: 0o755],
+            ofItemAtPath: hookPath.path
+        )
+
+        print("xmark: post-checkout hook installed at .git/hooks/post-checkout")
+        print("Run 'xmark setup' again in any other repo you want to enable.")
     }
 }
