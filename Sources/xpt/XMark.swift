70aa41c2af2910bead06476dd698376c7273dd57
Jon Bauer <jon@covelop.org>
2026-03-07 21:40:10 -0800

    Bump version to 0.2.0

--- Sources/xpt/XMark.swift
+++ Sources/xpt/XMark.swift
@@ -5,7 +5,7 @@ struct Xpt: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "xpt",
         abstract: "Save and restore per-branch Xcode breakpoints.",
-        version: "0.1.0",
+        version: "0.2.0",
         subcommands: [
             Setup.self,
             Save.self,
